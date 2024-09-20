34f2335830a8cc3888e158b9aef62bb57f542a2a
Ahmad Mimna'i Fiddin <ahmadfiddin687@gmail.com>
2024-09-20 23:13:14 +0700

    Pertemuan 04 - Praktikum 2.4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -18,5 +18,5 @@ class UserModel extends Model
      */
 
     // protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
