2b28106df41a2fd5e777f5f6ca98761525f7ea57
Ahmad Mimna'i Fiddin <ahmadfiddin687@gmail.com>
2024-09-20 22:05:14 +0700

    Pertemuan 04 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -9,6 +9,14 @@ class UserModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_user';    // mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; // mendefinisikan primary key dari tabel yang digunakan
+    protected $table = 'm_user';    
+    protected $primaryKey = 'user_id'; 
+
+    /**
+     * 
+     * @var array
+     */
+
+    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
