ca3adb5e8018a3484fc3bf01c0bd5cce56dcd5f6
Ahmad Mimna'i Fiddin <ahmadfiddin687@gmail.com>
2024-09-21 00:48:31 +0700

    Pertemuan 04 - Praktikum 2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::all();
+        $user = UserModel::with('level')->get();
         return view('user', ['data' => $user]);
     }
 
@@ -49,7 +49,7 @@ class UserController extends Controller
     }
 
     public function hapus($id) 
-        {
+    {
         $user = UserModel::find($id);
         $user->delete();
         return redirect('/user');
