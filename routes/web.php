ce744c9abb9d9919ed93d253ed6e46df3f1cea9a
Ahmad Mimna'i Fiddin <ahmadfiddin687@gmail.com>
2024-10-23 00:02:42 +0700

    Pertemuan 08 - Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -109,6 +109,8 @@ Route::middleware(['auth'])->group(function()
         Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']); // menyimpan perubahan data barang Ajax
         Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']); // untuk tampilkan form confirm delete barang Ajax
         Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // untuk hapus data barang Ajax 
+        Route::get('/import', [BarangController::class, 'import']); // ajax fprm upload excel
+        Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
         Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data barang
     });
 });
